569c7b360d683356f6d3a6ac8d07eb41208e8232
aurelieDANIEL2003 <aure_d7@hotmail.com>
2025-01-22 20:57:27 +0100

    0501

--- StreamlitE.py
+++ StreamlitE.py
@@ -2,12 +2,17 @@ import streamlit as st
 import pandas as pd
 import requests
 import warnings
+from utils1 import enlever_accents
 
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 # Charger le fichier CSV contenant les départements et villes
 df_loc1 = pd.read_csv('df_loc.csv')
+
+# Normaliser les colonnes pour la recherche insensible aux accents
+df_loc1["nom_departement_normalise"] = df_loc1["nom_departement"].apply(enlever_accents).str.lower()
+df_loc1["nom_ville_normalise"] = df_loc1["nom_ville"].apply(enlever_accents).str.lower()
 
 # Clé API Yelp
 API_KEY = "-rNtfI2lxUWH6YHZBqnAeYQ9rzpcAAn8dT-wshmTRxwON7ytBFNjVq8hbt527dD_G2XnF5BLApATmZmcTP0RwxLxqjc5MnBGfFY0Rb03fJ3Y9LJwvAn4fLIgAY5_Z3Yx"
@@ -20,10 +25,11 @@ st.title("Recherche de département, ville et restaurants en France")
 
 # Entrée pour rechercher un département par nom ou numéro
 query = st.text_input("Entrez le nom ou le numéro du département :").lower()
+query_normalise = enlever_accents(query.lower())  # Normaliser la requête
 
 # Filtrer les départements correspondant à la requête
 filtered_departments = df_loc1[
-    df_loc1["nom_departement"].str.lower().str.contains(query) | 
+    df_loc1["nom_departement_normalise"].str.contains(query_normalise) | 
     df_loc1["department_code"].str.contains(query)
 ]
 
@@ -49,10 +55,11 @@ if not filtered_departments.empty:
 
     # Rechercher des restaurants via l'API Yelp
     params = {
+        "state":selected_department,
         "location": selected_city,  # Ville sélectionnée
         "term": "restaurants",      # Recherche de restaurants
         "limit": 10,                # Nombre de résultats à récupérer
-        "sort_by": "rating",        # Trier par note
+        #"sort_by": "rating",        # Trier par note
     }
     headers = {
         "Authorization": f"Bearer {API_KEY}"
@@ -71,10 +78,14 @@ if not filtered_departments.empty:
                 name = business.get("name", "Nom indisponible")
                 address = ", ".join(business["location"].get("display_address", []))
                 rating = business.get("rating", "Non disponible")
+                review_count = business.get("review_count", "Non disponible")
+                image_url = business.get("image_url", "Non disponible")
                 phone = business.get("display_phone", "Non disponible")
                 st.write(f"- **{name}**")
+                st.image(f"{image_url}", width=150)
                 st.write(f"  - Adresse : {address}")
                 st.write(f"  - Note : {rating} ⭐")
+                st.write(f" - Nb Vote : {review_count}")
                 st.write(f"  - Téléphone : {phone}")
                 st.write("---")
         else:
