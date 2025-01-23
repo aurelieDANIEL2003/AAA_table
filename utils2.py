7db3621ab3f110854ab9533c6a17bc9e321f17ec
aurelieDANIEL2003 <aure_d7@hotmail.com>
2025-01-23 17:21:26 +0100

    230125

--- utils2.py
+++ utils2.py
@@ -6,4 +6,19 @@ import pandas as pd
 def lien_google(name, location):
     restaurant = name.replace(" ", "+")+ "+" + location.replace(" ", "+")
     url_restaurant = f"https://www.google.com/search?q={restaurant}"
+
+    html = requests.get(url_restaurant)
+    soup = BeautifulSoup(html.text, 'html.parser')
+
+       # identification comme navigateur
+    navigator = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'
+    html = requests.get(url_restaurant, headers={'User-Agent': navigator})
+
+        # Nous filtrons maintenant uniquement sur avis google sur la page :
+    soup_director = soup.find_all('span', {"class" : "Aq14fc"})
+    # # Puis nous cherchons tous les liens contenus dans cet encadré :
+    # links = soup_director.find_all('')
+
+
     return url_restaurant
+
