393b3148b63dc720349eada6e5cd43da9ba9cb6e
aurelieDANIEL2003 <aure_d7@hotmail.com>
2025-02-15 17:09:22 +0100

    150225

--- .streamlit/utils8.py
+++ .streamlit/utils8.py
@@ -3,16 +3,21 @@ import streamlit as st
 
 def fond(side_bg):
 
-   side_bg_ext = 'jpg'
+    side_bg_ext = 'png'
 
-   st.markdown(
-      f"""
-      <style>
-      [data-testid="stSidebar"] > div:first-child {{
-          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
-      }}
-      </style>
-      """,
-      unsafe_allow_html=True,
-      )
+    # Lecture et encodage de l'image
+    with open(side_bg, "rb") as file:
+        base64_img = base64.b64encode(file.read()).decode()
 
+    # Ajout du CSS pour rendre l'image responsive
+    st.markdown(
+        f"""
+        <style>
+        [data-testid="stSidebar"] > div:first-child {{
+            background: url(data:image/{side_bg_ext};base64,{base64_img}) no-repeat center center;
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
